6eeea96b93ee35b154ee3f7b200fa2534efe09ab
Menooker <myjisgreat@live.cn>
2018-08-09 16:13:49 +0800

    add bridge code

--- src/face_detect.py
+++ src/face_detect.py
@@ -1,18 +1,28 @@
 import cv2
 from mtcnn.mtcnn import MTCNN
 import matplotlib.pyplot as plt
+import numpy as np
 
-detector = MTCNN()
-file_path = '1.png'
-image = cv2.imread(file_path)
-boxes = detector.detect_faces(image)
-if len(boxes) != 1:
-    pass
-else:
-    face_position = boxes[0]['box']
-    print(face_position)
-    crop = image[face_position[1]:face_position[1] + face_position[3],
-                   face_position[0]: face_position[0] + face_position[2]]
-    cv2.imwrite('{0}.png'.format(file_path.split('.')[0] + '_' + 'face'), crop)
-    cv2.imshow("Mouth1", crop)
-    cv2.waitKey(0)
+def get_np_array_from_file_obj(fileobj):
+    '''converts a buffer from a tar file in np.array'''
+    return np.asarray(
+        bytearray(fileobj.read())
+        , dtype=np.uint8)
+
+def detect(imgfile):
+    detector = MTCNN()
+    image = cv2.imdecode(
+        get_np_array_from_file_obj(imgfile)
+        , cv2.IMREAD_UNCHANGED )
+    cv2.imwrite('testout.png', image)
+    boxes = detector.detect_faces(image)
+    ret=[]
+    for box in boxes:
+        face_position = box['box']
+        print(face_position)
+        crop = image[face_position[1]:face_position[1] + face_position[3],
+                       face_position[0]: face_position[0] + face_position[2]]
+        ret.append({'box':face_position,'gender':0,'age':25})
+        #cv2.imwrite('{0}.png'.format(file_path.split('.')[0] + '_' + 'face'), crop)
+    return ret
+
